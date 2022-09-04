6dac7fe12e57e5e512407a8a81cf18ce88ccc358
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-09-04 17:01:58 +0430

    Update __mainRunnerBuyPrimaryMACD__.py

--- __mainRunnerBuyPrimaryMACD__.py
+++ __mainRunnerBuyPrimaryMACD__.py
@@ -1,10 +1,9 @@
-import src
-from src.Indicators.MACD import Trader
 from GeneticLearningRunner.MACD import (
 										Runner_BuyPrimary,
 										Runner_BuySecondry,
 										Runner_SellPrimary,
 										Runner_SellSecondry,
 										)
+from src.Indicators.MACD import GetPermit_HowToUse
 
-Runner_BuyPrimary.Run()
+# Runner_BuyPrimary.Run()
