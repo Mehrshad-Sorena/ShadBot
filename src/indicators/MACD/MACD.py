3e031bc9954caf7711b77e7dcf398f0ceab7a12a
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-09-04 23:37:00 +0430

    Update MACD.py

--- src/indicators/MACD/MACD.py
+++ src/indicators/MACD/MACD.py
@@ -857,7 +857,7 @@ class MACD:
 									 																				signaltype = signaltype, 
 									 																				signalpriority = signalpriority
 									 																				)
-		print(ind_parameters.elements)
+		print(ind_parameters)
 		ind_config = indicator_config()
 		macd = Divergence(parameters = ind_parameters, config = ind_config)
 
