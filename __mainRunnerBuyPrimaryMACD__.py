814998e6f11ec31b24a2583337b2283c395ebcb9
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-09-04 16:41:31 +0430

    Create __mainRunnerBuyPrimaryMACD__.py

---
+++ __mainRunnerBuyPrimaryMACD__.py
@@ -0,0 +1,10 @@
+import src
+from src.Indicators.MACD import Trader
+from GeneticLearningRunner.MACD import (
+										Runner_BuyPrimary,
+										Runner_BuySecondry,
+										Runner_SellPrimary,
+										Runner_SellSecondry,
+										)
+
+Runner_BuyPrimary.Run()
