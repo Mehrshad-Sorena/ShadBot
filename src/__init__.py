1bce33b3e3f8af4bb004e79fb6b28060a2bd2ed7
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-09-04 13:55:17 +0430

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,23 @@
+from src.Indicators.MACD import (
+								MACD,
+								Parameters,
+								Config,
+								Chromosome,
+								ParameterLimits,
+								)
+
+from src.Indicators.StochAstic import (
+										StochAstic,
+										Parameters,
+										Config,
+										Chromosome,
+										ParameterLimits,
+										)
+
+from src.Indicators.RSI import (
+								RSI,
+								Parameters,
+								Config,
+								Chromosome,
+								ParameterLimits,
+								)
