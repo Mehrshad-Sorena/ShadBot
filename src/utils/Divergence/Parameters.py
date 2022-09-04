732cd40f033bf8bc0fb4a1c502d83f7642e3fd2a
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-09-04 15:44:21 +0430

    Update Parameters.py

--- src/utils/Divergence/Parameters.py
+++ src/utils/Divergence/Parameters.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from pr_Parameters import Parameters as pr_Parameters
+from src.Utils.ProtectResist.PRMethod.Parameters import Parameters as pr_Parameters
 
 class Parameters:
 
