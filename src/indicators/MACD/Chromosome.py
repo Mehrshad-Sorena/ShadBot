746b9de5ff35b9649e3931dc08a5fcb1d439e61b
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-09-04 19:15:10 +0430

    Update Chromosome.py

--- src/indicators/MACD/Chromosome.py
+++ src/indicators/MACD/Chromosome.py
@@ -1,5 +1,5 @@
 from .Config import Config as MACDConfig
-from ...Utils.Divergence.Parameters import Parameters as IndicatorParameters
+from Divergence.Parameters import Parameters as IndicatorParameters
 from scipy.stats import foldnorm, dweibull, rayleigh, expon, nakagami, norm
 from fitter import Fitter, get_common_distributions, get_distributions
 from sklearn.cluster import KMeans
