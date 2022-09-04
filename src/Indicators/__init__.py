9a0be743091bb99daa91d5ef7e95989fc843aa04
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-09-04 17:53:06 +0430

    Update __init__.py

--- src/Indicators/__init__.py
+++ src/Indicators/__init__.py
@@ -1,5 +1,8 @@
-from src.Indicators import (
-							MACD,
-							StochAstic,
-							RSI,
-							)
+# from .Indicators import (
+# 							MACD,
+# 							StochAstic,
+# 							RSI,
+# 							)
+from . import MACD
+from . import StochAstic
+from . import RSI
