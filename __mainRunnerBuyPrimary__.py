cb29d6a96fccb71f5d1a94ded2c848bdcd5d3c09
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-09-06 20:04:33 +0430

    Update __mainRunnerBuyPrimary__.py

--- __mainRunnerBuyPrimary__.py
+++ __mainRunnerBuyPrimary__.py
@@ -1,5 +1,7 @@
-from GeneticLearningRunner.MACD import Runner_BuyPrimary as MACDRunner
 from GeneticLearningRunner.StochAstic import Runner_BuyPrimary as StochAsticRunner
+from GeneticLearningRunner.MACD import Runner_BuyPrimary as MACDRunner
+from GeneticLearningRunner.RSI import Runner_BuyPrimary as RSIRunner
 
-MACDRunner.Run()
-StochAsticRunner.Run()
+# MACDRunner.Run()
+# StochAsticRunner.Run()
+RSIRunner.Run()
