edae5f58a8de1f7a3843680cef9dfec799f1aeaf
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-09-06 17:15:44 +0430

    Delete __mainRuunerBuyPrimary__.py

--- __mainRuunerBuyPrimary__.py
+++
@@ -1,5 +0,0 @@
-from GeneticLearningRunner.MACD import Runner_BuyPrimary as MACDRunner
-from GeneticLearningRunner.StochAstic import Runner_BuyPrimary as StochAsticRunner
-
-MACDRunner.Run()
-StochAsticRunner.Run()
