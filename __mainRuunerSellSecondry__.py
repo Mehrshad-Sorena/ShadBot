8a4cb900ac80d6c8b4010884de2f9c238a79e218
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-09-06 17:15:52 +0430

    Delete __mainRuunerSellSecondry__.py

--- __mainRuunerSellSecondry__.py
+++
@@ -1,5 +0,0 @@
-from GeneticLearningRunner.MACD import Runner_SellSecondry as MACDRunner
-from GeneticLearningRunner.StochAstic import Runner_SellSecondry as StochAsticRunner
-
-MACDRunner.Run()
-StochAsticRunner.Run()
