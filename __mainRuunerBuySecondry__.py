9525febc1a439be797390b45e9249a7605633135
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-09-06 17:05:10 +0430

    Create __mainRuunerBuySecondry__.py

---
+++ __mainRuunerBuySecondry__.py
@@ -0,0 +1,5 @@
+from GeneticLearningRunner.StochAstic import Runner_BuySecondry as StochAsticRunner
+from GeneticLearningRunner.MACD import Runner_BuySecondry as MACDRunner
+
+MACDRunner.Run()
+StochAsticRunner.Run()
