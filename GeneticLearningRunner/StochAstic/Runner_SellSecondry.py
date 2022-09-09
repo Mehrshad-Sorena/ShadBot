28a837fa38ebbff3b91b5f122045b415f3cf1c9e
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-09-09 23:22:17 +0430

    Update Runner_SellSecondry.py

--- GeneticLearningRunner/StochAstic/Runner_SellSecondry.py
+++ GeneticLearningRunner/StochAstic/Runner_SellSecondry.py
@@ -6,6 +6,7 @@ import pandas as pd
 from src.utils.Divergence.Parameters import Parameters as IndicatorParameters
 from src.utils.Divergence.Config import Config as IndicatorConfig
 from src.utils.Divergence.Divergence import Divergence
+from src.utils.Optimizers import NoiseCanceller
 from src.utils.Optimizers import Optimizers
 import os
 import numpy as np
@@ -22,6 +23,20 @@ def Run():
 	parameters.elements['symbol'] = 'XAUUSD_i'
 	parameters.elements['MACD_apply_to'] = 'close'
 
+	dataset_5M_real, _ = loging.readall(symbol = 'XAUUSD_i', number_5M = 'all', number_1H = 0)
+	dataset_5M_real = dataset_5M_real[parameters.elements['symbol']]
+
+	noise_canceller = NoiseCanceller.NoiseCanceller()
+	parameters.elements['dataset_5M']['XAUUSD_i']['close'] = noise_canceller.NoiseWavelet(dataset = parameters.elements['dataset_5M']['XAUUSD_i'], applyto = 'close')
+	parameters.elements['dataset_5M']['XAUUSD_i']['open'] = noise_canceller.NoiseWavelet(dataset = parameters.elements['dataset_5M']['XAUUSD_i'], applyto = 'open')
+	parameters.elements['dataset_5M']['XAUUSD_i']['high'] = noise_canceller.NoiseWavelet(dataset = parameters.elements['dataset_5M']['XAUUSD_i'], applyto = 'high')
+	parameters.elements['dataset_5M']['XAUUSD_i']['low'] = noise_canceller.NoiseWavelet(dataset = parameters.elements['dataset_5M']['XAUUSD_i'], applyto = 'low')
+	parameters.elements['dataset_5M']['XAUUSD_i']['HL/2'] = noise_canceller.NoiseWavelet(dataset = parameters.elements['dataset_5M']['XAUUSD_i'], applyto = 'HL/2')
+	parameters.elements['dataset_5M']['XAUUSD_i']['HLC/3'] = noise_canceller.NoiseWavelet(dataset = parameters.elements['dataset_5M']['XAUUSD_i'], applyto = 'HLC/3')
+	parameters.elements['dataset_5M']['XAUUSD_i']['HLCC/4'] = noise_canceller.NoiseWavelet(dataset = parameters.elements['dataset_5M']['XAUUSD_i'], applyto = 'HLCC/4')
+	parameters.elements['dataset_5M']['XAUUSD_i']['OHLC/4'] = noise_canceller.NoiseWavelet(dataset = parameters.elements['dataset_5M']['XAUUSD_i'], applyto = 'OHLC/4')
+
+
 	optimizers = Optimizers.Optimizers() 
 
 	optimizers.symbol = 'XAUUSD_i'
@@ -36,6 +51,7 @@ def Run():
 	stochastic = StochAstic(parameters = parameters, config = config)
 	try:
 		stochastic_calc = stochastic.Genetic(
+											dataset_5M_real = dataset_5M_real,
 											dataset_5M = parameters.elements['dataset_5M'], 
 											dataset_1H = parameters.elements['dataset_1H'], 
 											symbol = 'XAUUSD_i',
@@ -49,6 +65,7 @@ def Run():
 	for turn in range(0,4):
 		try:
 			stochastic_calc = stochastic.GetPermit(
+												dataset_5M_real = dataset_5M_real,
 												dataset_5M = parameters.elements['dataset_5M'],
 												dataset_1H = parameters.elements['dataset_1H'], 
 												symbol = 'XAUUSD_i',
