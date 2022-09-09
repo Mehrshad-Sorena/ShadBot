c94a52d65cfdb976f611255ebb3e5f621cb88ed6
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-09-09 23:21:42 +0430

    Update Runner_BuyPrimary.py

--- GeneticLearningRunner/MACD/Runner_BuyPrimary.py
+++ GeneticLearningRunner/MACD/Runner_BuyPrimary.py
@@ -7,9 +7,11 @@ from src.utils.Divergence.Parameters import Parameters as IndicatorParameters
 from src.utils.Divergence.Config import Config as IndicatorConfig
 from src.utils.Divergence.Divergence import Divergence
 from src.utils.Optimizers import Optimizers
+from src.utils.Optimizers import NoiseCanceller
 import os
 import numpy as np
 import pandas as pd
+import sys
 
 def Run():
 	loging = getdata()
@@ -21,7 +23,23 @@ def Run():
 	parameters.elements['symbol'] = 'XAUUSD_i'
 	parameters.elements['MACD_apply_to'] = 'close'
 
+	dataset_5M_real, _ = loging.readall(symbol = 'XAUUSD_i', number_5M = 'all', number_1H = 0)
+	dataset_5M_real = dataset_5M_real[parameters.elements['symbol']]
+
 	optimizers = Optimizers.Optimizers() 
+
+	noise_canceller = NoiseCanceller.NoiseCanceller()
+	parameters.elements['dataset_5M']['XAUUSD_i']['close'] = noise_canceller.NoiseWavelet(dataset = parameters.elements['dataset_5M']['XAUUSD_i'], applyto = 'close')
+	parameters.elements['dataset_5M']['XAUUSD_i']['open'] = noise_canceller.NoiseWavelet(dataset = parameters.elements['dataset_5M']['XAUUSD_i'], applyto = 'open')
+	parameters.elements['dataset_5M']['XAUUSD_i']['high'] = noise_canceller.NoiseWavelet(dataset = parameters.elements['dataset_5M']['XAUUSD_i'], applyto = 'high')
+	parameters.elements['dataset_5M']['XAUUSD_i']['low'] = noise_canceller.NoiseWavelet(dataset = parameters.elements['dataset_5M']['XAUUSD_i'], applyto = 'low')
+	parameters.elements['dataset_5M']['XAUUSD_i']['HL/2'] = noise_canceller.NoiseWavelet(dataset = parameters.elements['dataset_5M']['XAUUSD_i'], applyto = 'HL/2')
+	parameters.elements['dataset_5M']['XAUUSD_i']['HLC/3'] = noise_canceller.NoiseWavelet(dataset = parameters.elements['dataset_5M']['XAUUSD_i'], applyto = 'HLC/3')
+	parameters.elements['dataset_5M']['XAUUSD_i']['HLCC/4'] = noise_canceller.NoiseWavelet(dataset = parameters.elements['dataset_5M']['XAUUSD_i'], applyto = 'HLCC/4')
+	parameters.elements['dataset_5M']['XAUUSD_i']['OHLC/4'] = noise_canceller.NoiseWavelet(dataset = parameters.elements['dataset_5M']['XAUUSD_i'], applyto = 'OHLC/4')
+
+	print('Data Ready .... ')
+	# sys.exit()
 
 	optimizers.symbol = 'XAUUSD_i'
 	optimizers.sigpriority = 'primary'
@@ -38,6 +56,7 @@ def Run():
 
 	try:
 		macd_calc = macd.Genetic(
+								dataset_5M_real = dataset_5M_real,
 								dataset_5M = parameters.elements['dataset_5M'], 
 								dataset_1H = parameters.elements['dataset_1H'], 
 								symbol = 'XAUUSD_i',
@@ -50,9 +69,10 @@ def Run():
 		print('MACD ERROR: ', ex)
 
 	for turn in range(0,4):
-
+		print('GetPermit ... ')
 		try:
 			macd_calc = macd.GetPermit(
+									dataset_5M_real = dataset_5M_real,
 									dataset_5M = parameters.elements['dataset_5M'],
 									dataset_1H = parameters.elements['dataset_1H'], 
 									symbol = 'XAUUSD_i',
@@ -60,6 +80,7 @@ def Run():
 									signalpriority = 'primary',
 									flag_savepic = False
 									)
+			print()
 
 			if (
 				macd_calc['draw_down'][0] <= 7 &
