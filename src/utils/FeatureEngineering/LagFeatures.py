d3627474278ac18c2ea0bdc7f559158f1605dd06
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-09-22 19:31:09 +0330

    Update LagFeatures.py

--- src/utils/FeatureEngineering/LagFeatures.py
+++ src/utils/FeatureEngineering/LagFeatures.py
@@ -1,3 +1,4 @@
+from src.utils.FeatureEngineering.Frequencies import Frequencies
 from statsmodels.tsa.stattools import acf
 import pandas as pd
 
@@ -25,7 +26,7 @@ class LagFeatures():
 
 		for corr in correlations:
 
-			if len(self.lags) > 10: break
+			if len(self.lags) > 4: break
 
 			if corr > 0.97:
 
@@ -34,36 +35,64 @@ class LagFeatures():
 
 			lag_counter += 1
 
-	def LagCreation(self, dataset):
+	def LagCreation(self, dataset, symbol):
 
 		dataset_lag = dataset.copy(deep = True)
 		dataset_lag.index = pd.to_datetime(dataset_lag['time_5m'])
 		dataset_lag = dataset_lag.drop(columns = ['time_5m', 'time_1h'])
 
-		dataset_frequented = dataset_lag.resample('20T').last()
-
-		self.LagFinder(dataset = dataset_frequented.reset_index().copy(deep = True), applyto = 'close_5m')
-
 		outlier_cutoff = 0.01
 		LagedData = pd.DataFrame()
 
-		for lag in self.lags:
-			LagedData[f'return_{lag}'] = (dataset_frequented
-														.pct_change(lag)
-														.stack()
-														# .pipe(lambda x:
-														# 				x.clip(
-														# 						lower=x.quantile(outlier_cutoff),
-														# 						upper=x.quantile(1-outlier_cutoff)
-														# 						)
-														# 	)
-														# .add(1)
-														# .pow(1/lag)
-														# .sub(1)
-											)
+		frequences = Frequencies()
+		frequences = frequences.Get(dataset = dataset, symbol = symbol, mode = None, number_frequencies = 2)
 
-		LagedData['real'] = (dataset_frequented.stack())
+		LagedData[f'real_{0}'] = (dataset_lag.copy(deep = True).stack())
+
+		for freq_counter in range(0, 2):
+
+			freq = frequences['freq_close_5m'][freq_counter]
+			dataset_frequented = pd.DataFrame()
+			dataset_frequented = dataset_lag.copy(deep = True).resample(str(freq) + 'T').last().dropna(subset=['close_5m'])
+			self.LagFinder(dataset = dataset_frequented.reset_index().copy(deep = True), applyto = 'close_5m')
+
+			for lag in self.lags:
+
+				LagedData[f'return_{freq}_{lag}'] = (dataset_frequented
+																	.pct_change(lag)
+																	.stack()
+																	# .pipe(lambda x:
+																	# 				x.clip(
+																	# 						lower=x.quantile(outlier_cutoff),
+																	# 						upper=x.quantile(1-outlier_cutoff)
+																	# 						)
+																	# 	)
+																	# .add(1)
+																	# .pow(1/lag)
+																	# .sub(1)
+													)
+
+			LagedData = self.MemontumCreation(dataset = LagedData, freq = freq)
+			LagedData[f'real_{freq}'] = (dataset_frequented.stack())
 
 		LagedData = LagedData.swaplevel()
 
 		return LagedData
+
+
+	def MemontumCreation(self, dataset, freq):
+
+		for lag in self.lags:
+			dataset[f'momentum_{freq}_{lag}'] = dataset[f'return_{freq}_{lag}'].sub(dataset[f'return_{freq}_{self.lags[0]}'])
+
+		return dataset
+
+	def LagShiftedCreation(self, dataset):
+
+		for t in self.timelags:
+			dataset[f'target_-{t}h'] = (dataset[f'return_{t}h'].shift(t))
+
+		for t in self.lags:
+			dataset[f'target_{t}h'] = (dataset[f'return_{t}h'].shift(-t))
+		
+		return dataset
