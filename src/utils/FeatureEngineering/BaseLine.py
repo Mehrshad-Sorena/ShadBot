a651ae304ea2d378ba449a52307a31d908e50147
Mehrshad-Sorena <68142233+Mehrshad-Sorena@users.noreply.github.com>
2022-09-28 00:15:19 +0330

    Create BaseLine.py

---
+++ src/utils/FeatureEngineering/BaseLine.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+
+class BaseLine(tf.keras.Model):
+
+  def __init__(self, label_index=None):
+
+    super().__init__()
+    self.label_index = label_index
+
+
+  def call(self, inputs):
+
+    if self.label_index is None:
+      return inputs
+
+    result = inputs[:, :, self.label_index]
+
+    return result[:, :, tf.newaxis]
